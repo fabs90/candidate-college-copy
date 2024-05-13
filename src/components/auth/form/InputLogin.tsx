dc696bb1242c3877ec624ad14c6f3ddfd738d266
ashafaatadhis <skullreaper387@gmail.com>
2024-05-13 15:44:33 +0700

    setting mutate tanstack query

--- src/components/auth/form/InputLogin.tsx
+++ src/components/auth/form/InputLogin.tsx
@@ -1,16 +1,20 @@
-import { HTMLInputTypeAttribute } from "react";
+import { Dispatch, HTMLInputTypeAttribute, SetStateAction } from "react";
 
 export default function InputLogin({
   name,
   id,
   type,
   placeholder,
+  value,
+  setValue,
   className,
 }: {
   name: string;
   id: string;
   type: HTMLInputTypeAttribute;
   placeholder: string;
+  value: string;
+  setValue: Dispatch<SetStateAction<string>>;
   className: string;
 }) {
   return (
@@ -20,8 +24,13 @@ export default function InputLogin({
       </label>
       <input
         id={id}
+        name={id}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
         className="bg-transparent mt-2 w-full text-sm rounded-[4px] py-[18px] px-[16px] outline-none border border-grey"
         type={type}
+        value={value}
         placeholder={placeholder}
       />
     </div>
--- src/components/auth/section/FormSection.tsx
+++ src/components/auth/section/FormSection.tsx
@@ -1,42 +1,68 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import InputLogin from "../form/InputLogin";
 import RememberMe from "../form/RememberMe";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import useLoginForm from "@/hooks/useLoginForm";
 
 export default function FormSection() {
-  const [rememberme, setRememberme] = useState<boolean>(false);
+  const {
+    email,
+    password,
+    rememberme,
+    setEmail,
+    mutation,
+    setPassword,
+    setRememberme,
+    onSubmit,
+  } = useLoginForm();
+
   return (
     <section className="flex-1">
-      <div className="flex w-full h-full max-w-[70%] m-auto flex-col justify-center   ">
-        <h2 className="text-center text-3xl font-medium">
+      <div className="flex w-full h-full max-w-[80%] lg:max-w-[70%] m-auto flex-col justify-center   ">
+        <h2 className="text-center  text-3xl font-medium">
           Welcome to CC Weekly Report
         </h2>
-        <p className="text-grey mt-3 text-center text-xl font-medium">
+
+        <p className="text-grey mt-3 text-center  text-xl   font-medium">
           Login with your account to get started
         </p>
-        <div className="max-w-[80%] w-full mx-auto">
-          <InputLogin
-            className="mt-12 w-full"
-            id="email"
-            name="Email"
-            placeholder="Enter your email"
-            type="email"
-          />
-          <InputLogin
-            className="mt-5 w-full"
-            id="password"
-            name="Password"
-            placeholder="Enter your password"
-            type="password"
-          />
+        <div className="max-w-[90%] lg:max-w-[75%] w-full mx-auto">
+          <form onSubmit={onSubmit}>
+            <InputLogin
+              className="mt-12 w-full"
+              id="email"
+              value={email}
+              setValue={setEmail}
+              name="Email"
+              placeholder="Enter your email"
+              type="email"
+            />
+            <InputLogin
+              className="mt-5 w-full"
+              value={password}
+              id="password"
+              setValue={setPassword}
+              name="Password"
+              placeholder="Enter your password"
+              type="password"
+            />
 
-          <div className="flex justify-between mt-5">
-            <RememberMe rememberme={rememberme} setRememberme={setRememberme} />
-            <span className="text-sm text-secondary">Forgot Password?</span>
-          </div>
+            <div className="flex justify-between mt-5">
+              <RememberMe
+                rememberme={rememberme}
+                setRememberme={setRememberme}
+              />
+              <span className="text-sm text-secondary">Forgot Password?</span>
+            </div>
 
-          <button className="w-full mt-12 bg-secondary text-primary rounded-[30px] font-semibold py-4 text-center">
-            Login
-          </button>
+            <button
+              type="submit"
+              className="w-full mt-12 bg-secondary text-primary rounded-[30px] font-semibold py-4 text-center"
+            >
+              Login
+            </button>
+          </form>
           <p className="font-medium mt-3 text-sm text-center">
             Don’t Have An Account?{" "}
             <span className="text-secondary">Contact Your Admin</span>
